bb757992ec1efe929cf682803459e72c172071b1
Denys Bondar <denys.bondar@israelit.pro>
2022-07-18 18:01:05 +0300

    fix cs

--- algluna.rb
+++ algluna.rb
@@ -4,26 +4,28 @@ print 'Hi! Enter your number card? '
 numbers_card = gets.chomp
 
 def valid_card(numbers_card)
-  numbers_card = numbers_card.gsub(/[^\d]/, '')
-
+  numbers_card = numbers_card.gsub(/[^\d]/, '').split('').map(&:to_i)
   if numbers_card.empty?
     puts 'number_card is empty'
   else
-
     sum_hash = 0
-    numbers_card = numbers_card.split('').map(&:to_i)
-
-    numbers_card.each_with_index do |item, index|
-      if item.even?
-        val = numbers_card[index]
-      else
-        val = numbers_card[index] * 2
-        val -= 9 if val > 9
-      end
-      sum_hash += val
-    end
+    sum_hash = check_hash_sum(numbers_card, sum_hash)
+    puts sum_hash
     (sum_hash % 10).zero?
   end
+end
+
+def check_hash_sum(valid_numbers,  sum_hash = 0)
+  valid_numbers.each_with_index do |item, index|
+    if item.even?
+      val = valid_numbers[index]
+    else
+      val = valid_numbers[index] * 2
+      val -= 9 if val > 9
+    end
+    sum_hash += val
+  end
+  sum_hash
 end
 
 if valid_card(numbers_card)
